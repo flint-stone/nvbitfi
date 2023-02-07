59205f9b21b946161fb7ca1cc3103a4afbe87949
wisr <wisr@node3.of.wail.wisc.edu>
2023-02-07 12:21:19 -0600

    add opcode print injectior

---
+++ opcode_injector/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# export TOOL_VERBOSE=1; LD_PRELOAD=pf_injector.so ../test-apps/simple_add/simple_add;
+export TOOL_VERBOSE=1; LD_PRELOAD=pf_injector.so ../test-apps/vectoradd/vectoradd;
